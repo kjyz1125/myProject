23ce0d1fd5069051a6fe9a178b938c4d3a32e6b2
jykim <jykim@DESKTOP-NIPRLO3>
2020-02-28 14:54:21 +0900

    파일 업로드 운영환경 검토

--- distance_calculator/src/main/java/com/eljamdev/common/CommonMethod.java
+++ distance_calculator/src/main/java/com/eljamdev/common/CommonMethod.java
@@ -31,7 +31,7 @@ public class CommonMethod {
         // uuid 생성(Universal Unique IDentifier, 범용 고유 식별자)
         UUID uuid = UUID.randomUUID();
         // 랜덤생성+파일이름 저장
-        String savedName = uuid.toString()+FilenameUtils.getExtension(originalName);
+        String savedName = uuid.toString()+"."+FilenameUtils.getExtension(originalName);
         File target = new File(uploadPath, savedName);
         // 임시디렉토리에 저장된 업로드된 파일을 지정된 디렉토리로 복사
         // FileCopyUtils.copy(바이트배열, 파일객체)
